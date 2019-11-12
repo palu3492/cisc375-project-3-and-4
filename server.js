d4ae5937ccd20873fd9c40c07521fe374e5f2dba
Alex Palumbo <palu3492@stthomas.edu>
2019-11-11 18:02:17 -0600

    Update server.js
    Added /neighborhoods and /incidents GET handlers. 30/40 points done.

--- server.js
+++ server.js
@@ -19,31 +19,36 @@ let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
         console.log('Error opening ' + db_filename);
     } else {
         console.log('Now connected to ' + db_filename);
-        testSql();
+        //testSql();
     }
 });
 
+// SQL query testing function
 function testSql(){
-    db.all("SELECT * FROM Codes ORDER BY code ASC", (err, rows) => {
-        let code, incidentType, json;
-        let codes = {};
+    // incidents
+    db.all("SELECT * FROM Incidents", (err, rows) => {
+        let caseNumber, dateTime, date, time, json;
+        let incidents = {};
         rows.forEach(row => {
-            code = row['code'];
-            incidentType = row['incident_type'];
-            codes[code] = incidentType;
+            caseNumber = row['case_number'];
+            dateTime = row['date_time'];
+            [date, time] = dateTime.split('T');
+            incidents[caseNumber] = {
+                'date': date,
+                'time': time,
+                'code': row['code'],
+                'incident': row['incident'],
+                'police_grid': row['police_grid'],
+                'neighborhood_number': row['neighborhood_number'],
+                'block': row['block'],
+            };
         });
-        json = JSON.stringify(codes);
-        /*
-        [
-            { code: 603, incident_type: 'Theft - Mail/Package' },
-            { code: 603, incident_type: 'Theft - Mail/Package' },
-            { code: 603, incident_type: 'Theft - Mail/Package' }
-        ]
-        */
+        json = JSON.stringify(incidents);
     });
 }
 
 // GET /codes
+// Return JSON object with list of codes and their corresponding incident type (ordered by code)
 app.get('/codes', (req, res) => {
     db.all("SELECT * FROM Codes ORDER BY code ASC", (err, rows) => {
         let code, incidentType, json;
@@ -60,43 +65,52 @@ app.get('/codes', (req, res) => {
     });
 });
 
+// GET /neighborhoods
+// Return JSON object with list of neighborhood ids and their corresponding neighborhood name (ordered by neighborhood number)
+app.get('/neighborhoods', (req, res) => {
+    db.all("SELECT * FROM Neighborhoods ORDER BY neighborhood_number ASC", (err, rows) => {
+        let neighborhoodNumber, neighborhoodName, json;
+        let neighborhoods = {};
+        rows.forEach(row => {
+            neighborhoodNumber = row['neighborhood_number'];
+            neighborhoodName = row['neighborhood_name'];
+            neighborhoods[neighborhoodNumber] = neighborhoodName;
+        });
+        json = JSON.stringify(neighborhoods);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(json);
+        res.end();
+    });
+});
+
+// GET /incidents
+// Return JSON object with list of crime incidents (most recent first). Make date and time separate fields.
+app.get('/incidents', (req, res) => {
+    db.all("SELECT * FROM Incidents ORDER BY date_time DESC LIMIT 2", (err, rows) => {
+        let caseNumber, dateTime, date, time, json;
+        let incidents = {};
+        // The rows are ordered correctly by time but when putting them into object it won't order them correctly even after using rows.reverse()
+        rows.forEach(row => {
+            caseNumber = row['case_number'];
+            dateTime = row['date_time'];
+            [date, time] = dateTime.split('T');
+            incidents[caseNumber] = {
+                'date': date,
+                'time': time,
+                'code': row['code'],
+                'incident': row['incident'],
+                'police_grid': row['police_grid'],
+                'neighborhood_number': row['neighborhood_number'],
+                'block': row['block'],
+            };
+        });
+        json = JSON.stringify(incidents);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(json);
+        res.end();
+    });
+});
+
 
 console.log('Listening on port ' + port);
 let server = app.listen(port);
-/*
-GET /codes
-Return JSON object with list of codes and their corresponding incident type (ordered by code)
-{
-  "110": "Murder, Non Negligent Manslaughter",
-  "120": "Murder, Manslaughter By Negligence",
-  "210": "Rape, By Force"
-}
-
-
-GET /neighborhoods
-Return JSON object with list of neighborhood ids and their corresponding neighborhood name (ordered by neighborhood number)
-{
-  "1": "Conway/Battlecreek/Highwood",
-  "2": "Greater East Side",
-  "3": "West Side",
-  "4": "Dayton's Bluff"
-}
-
-
-GET /incidents
-Return JSON object with list of crime incidents (most recent first). Make date and time separate fields.
-Example:
-{
-  "19245020": {
-    "date": "2019-10-30",
-    "time": "23:57:08",
-    "code": 9954,
-    "incident": "Proactive Police Visit",
-    "police_grid": 87,
-    "neighborhood_number": 7,
-    "block": "THOMAS AV  & VICTORIA"
-  },
-  ...
-}
-
-*/
