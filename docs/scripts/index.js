548aef59d3da3d1120ab4477d75674527b361f6e
Alex Palumbo <palu3492@stthomas.edu>
2019-12-08 20:26:28 -0600

    Update
    Map lat and long connected to inputs

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -3,18 +3,29 @@
 let app;
 
 function init() {
+
     app = new Vue({
         el: "#app",
         data: {
-            spotify_search: "",
-            spotify_type: "artist",
-            spotify_type_options: [],
-            search_results: []
+            mapLatitude: 44.94317442551431,
+            mapLongitude: -93.10775756835939
         },
+        methods: {
+            latLngChange: function() {
+                console.log(this.mapLatitude);
+                leafletMap.panTo([this.mapLatitude, this.mapLongitude]);
+            }
+        }
     });
 
-    let stPaulLatLng = [44.9397629,-93.1410728]; // Latitude and longitude of St. Paul
-    let leafletMap = L.map('map').setView(stPaulLatLng, 13);
+    leafletMapInit();
+}
+
+let leafletMap;
+function leafletMapInit(){
+    // console.log(app);
+    let stPaulLatLng = [app.mapLatitude, app.mapLongitude]; // Latitude and longitude of St. Paul
+    leafletMap = L.map('map', {minZoom: 12, maxZoom: 17}).setView(stPaulLatLng, 12); // mouse click-and-drag and scroll wheel interaction available by default
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -22,4 +33,37 @@ function init() {
         id: 'mapbox/streets-v11',
         accessToken: 'pk.eyJ1IjoiYWpwMCIsImEiOiJjazN4cGd4MGQxNW1hM3F0NnU5M3Jiem80In0.71DleDv1Fm-ArumkU37BjA'
     }).addTo(leafletMap);
+
+    leafletMap.on('click', onMapClick);
+    leafletMap.on('move', onMapChange);
+    leafletMap.on('zoom', onMapChange);
+
+    addPolygon();
+}
+
+function onMapChange(){
+    let latLng = leafletMap.getCenter();
+    app.mapLatitude = latLng.lat;
+    app.mapLongitude = latLng.lng;
+}
+
+let popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(leafletMap);
+}
+
+function addPolygon(){
+    // Polygon for St. Paul
+    let polygon = L.polygon([
+        [44.987922, -93.207506],
+        [44.991685, -93.005289],
+        [44.891321, -93.004774],
+        [44.919406, -93.050779],
+        [44.919649, -93.128541],
+        [44.887429, -93.173517],
+        [44.909195, -93.202013]
+    ]).addTo(leafletMap);
 }
