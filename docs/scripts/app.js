e01979e8eec7bcf1b6e75400f50555034be46965
Alex Palumbo <palu3492@stthomas.edu>
2020-01-23 18:14:17 -0600

    Update
    website now works off of Heroku server

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -26,12 +26,12 @@ function init() {
             timeEnd: "",
             incidentFilter: [],
             neighborhoodFilter: [],
-            port: 8000,
-            portSupplied: false,
-            viewFilters: false,
+            // port: 8000,
+            // portSupplied: false,
+            // viewFilters: false,
             limit: 10000,
-            showNotification: false,
-            notification: ""
+            // showNotification: false,
+            // notification: ""
         },
         computed: {
             collapseImage: function(){
@@ -43,11 +43,7 @@ function init() {
         },
         methods: {
             toggleMenu: function(){
-                if(this.portSupplied) {
-                    this.showMenu = !this.showMenu;
-                } else {
-                    alert('Enter port!')
-                }
+                this.showMenu = !this.showMenu;
             },
             // When 'Go' is pressed
             changeLatLng: function() {
@@ -91,17 +87,19 @@ function init() {
                 // other (green)
                 return "color: #0e6500;"
             },
-            portSubmit: function(){
-                this.portSupplied = true;
-                getCodes();
-                getIncidents();
-            }
+            // portSubmit: function(){
+            //     this.portSupplied = true;
+            //     getCodes();
+            //     getIncidents();
+            // }
         }
     });
 
     createLeafletMap();
     populateNeighborhoods();
     neighborhoodUpdate();
+    getCodes();
+    getIncidents();
 }
 
 // function portSubmit(){
@@ -149,24 +147,24 @@ function onMapChange(){
     updateNeighborhoodsOnMap();
 }
 
-function updateAddress(){
-    let apiUrl = 'https://nominatim.openstreetmap.org/reverse?format=json&lat='+app.latitude+'&lon='+app.longitude+'&zoom=18&addressdetails=1';
-    $.getJSON(apiUrl)
-        .then(data => {
-            let addressParts = [];
-            if(data.address.house_number) {
-                addressParts.push(data.address.house_number);
-            }
-            if(data.address.road){
-                addressParts.push(data.address.road);
-            }
-            if(addressParts.length > 0) {
-                app.address = addressParts.join(' ');
-            }else{
-                app.address = 'No address';
-            }
-        })
-}
+// function updateAddress(){
+//     let apiUrl = 'https://nominatim.openstreetmap.org/reverse?format=json&lat='+app.latitude+'&lon='+app.longitude+'&zoom=18&addressdetails=1';
+//     $.getJSON(apiUrl)
+//         .then(data => {
+//             let addressParts = [];
+//             if(data.address.house_number) {
+//                 addressParts.push(data.address.house_number);
+//             }
+//             if(data.address.road){
+//                 addressParts.push(data.address.road);
+//             }
+//             if(addressParts.length > 0) {
+//                 app.address = addressParts.join(' ');
+//             }else{
+//                 app.address = 'No address';
+//             }
+//         })
+// }
 
 // Puts polygon around St. Paul on map
 function addBoundary(){
@@ -191,8 +189,9 @@ function addBoundary(){
     ], {fill: false, color: '#000'}).addTo(map);
 }
 
+let apiUrl = 'https://st-paul-crime-map.herokuapp.com';
 function getIncidents(){
-    let apiUrl = 'http://localhost:'+app.port+'/incidents?';
+    let incidentsApiUrl = apiUrl+'/incidents?';
     let filter = [];
     if(app.dateStart){
         let date = 'start_date='+app.dateStart;
@@ -220,9 +219,9 @@ function getIncidents(){
         filter.push('id='+app.neighborhoodFilter.join(','));
     }
     // add all filters together with '&'
-    apiUrl += filter.join('&');
-    console.log(apiUrl);
-    $.getJSON(apiUrl)
+    incidentsApiUrl += filter.join('&');
+    console.log(incidentsApiUrl);
+    $.getJSON(incidentsApiUrl)
         .then(data => {
             app.incidents = data;
             addCrimeAmounts();
@@ -246,8 +245,8 @@ function addCrimeAmounts(){
 }
 
 function getCodes(){
-    let apiUrl = 'http://localhost:'+app.port+'/codes';
-    $.getJSON(apiUrl)
+    let codesApiUrl = apiUrl+'/codes';
+    $.getJSON(codesApiUrl)
         .then(data => {
             for(let c in data){
                 app.codes[c.substring(1)] = data[c];
